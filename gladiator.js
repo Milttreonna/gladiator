ac81f87412fe76e7a48489dfbca07062a2b4e29d
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-11-18 16:01:07 -0600

    got buttons to work

--- gladiator.js
+++ gladiator.js
@@ -1,7 +1,42 @@
-function something() {alert("WE DID SOMETHING!");}
+// function something() {alert("IT DID SOMETHING!");}
+//
+//
+function attack1() {
+  var bar = document.getElementById("myBar2");
+  var width = 100; // determines where it starts at
+  var id = setInterval(frame,1000);
+  function frame() {
+    if (width <= 0) {
 
+        document.getElementById("content2").innerHTML='Queen Marceline <br/> Loses!';
+        document.getElementById("content1").innerHTML='Princess Bubblegum <br/> Wins!';
+      // break;
+    } else {
 
-var health = document.getElementById("health")
-var attack = document.getElementById("attack")
+      width -=(Math.floor((Math.random() * 20) + 10));
+      bar.style.width = width + '%';
+      document.getElementById("label2").innerHTML = width * 1  + '%';
+    }
+  }
+}
 
-function attack() { var attack = health.value - (Math.floor((Math.random() * 30) + 10));}
+function attack2() {
+  var bar = document.getElementById("myBar1");
+  var width = 100;
+  var id = setInterval(frame,1000);
+  function frame() {
+    if (width <= 0) {
+
+      document.getElementById("content1").innerHTML='Princess Bubblegum<br/> Loses!';
+      document.getElementById("content2").innerHTML='Queen Marceline <br/> Wins!';
+
+      // break;
+    } else {
+
+      width -=(Math.floor((Math.random() * 20) + 10));
+      bar.style.width = width + '%';
+      document.getElementById("label1").innerHTML = width * 1  + '%';
+    }
+
+  }
+}
