cb68f696f0e3cac0bed616867cec68eaca64e68f
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-12-02 11:25:48 -0600

    progress bar progression

--- gladiator2.js
+++ gladiator2.js
@@ -1,5 +1,5 @@
 window.addEventListener("keydown", checkKeyPressed1, false);
-// window.addEventListener("keydown", checkKeyPressed2, false);
+window.addEventListener("keydown", checkKeyPressed2, false);
 
 
 
@@ -10,19 +10,42 @@ function checkKeyPressed1(evnt) {
     var bar = document.getElementById("myBar2");
     var width = 100; // determines where it starts at
     if (evnt.keyCode == "65") {
-        width -= (Math.floor((Math.random() * 100) + 90));
+      var random=(Math.floor((Math.random() * 20) + 10))
+        width -= random;
         bar.style.width = width + '%'; // doesnt change it
         document.getElementById("label2").innerHTML = width * 1 + '%'; //doesnt change it
 
-        // var id = setInterval(frame1,1000);
-        function frame1() {
+        {
             if (width <= 0) {
                 // this is player 1 attacking player 2
                 document.getElementById("content2").innerHTML = 'Queen Marceline <br/> Loses!';
                 document.getElementById("content1").innerHTML = 'Princess Bubblegum <br/> Wins!';
             }
+
         }
-    }
+        }
+
+}
+
+function checkKeyPressed2(evnt) {
+    var bar = document.getElementById("myBar1");
+    var width = 100; // determines where it starts at
+    if (evnt.keyCode == "13") {
+      var random=(Math.floor((Math.random() * 20) + 10))
+        width -= random;
+        bar.style.width = width + '%'; // doesnt change it
+        document.getElementById("label1").innerHTML = width * 1 + '%'; //doesnt change it
+
+        {
+            if (width <= 0) {
+                // this is player 1 attacking player 2
+                document.getElementById("content2").innerHTML = 'Princess Bubblegum <br/> Loses!';
+                document.getElementById("content1").innerHTML = 'Queen Marceline <br/> Wins!';
+            }
+
+        }
+        }
+
 }
 
 //
