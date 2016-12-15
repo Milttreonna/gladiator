e9efde636200e621516edcb3c863f881dc758b64
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-12-15 14:36:43 -0600

    fixed

--- gladiator.js
+++ gladiator.js
@@ -52,3 +52,29 @@ function checkKeyPressed2(evnt) {
 
   }}
 }
+
+//
+// // function attack2() {
+// function checkKeyPressed2(evnt) {
+//     if (evnt.keyCode == "13") {
+//
+//   var bar = document.getElementById("myBar1");
+//   var width = 100;
+//   var id = setInterval(frame2,1000);
+//   function frame2() {
+//     if (width <= 0) {
+// // this is player 2 attacking player1
+//       document.getElementById("content1").innerHTML='Princess Bubblegum<br/> Loses!';
+//       document.getElementById("content2").innerHTML='Queen Marceline <br/> Wins!';
+//       // clearInterval(frame1);
+//       // break;
+//     } else {
+//
+//       width -=(Math.floor((Math.random() * 20) + 10));
+//       bar.style.width = width + '%';
+//       document.getElementById("label1").innerHTML = width * 1  + '%';
+//       clearInterval(frame1);
+//     }
+//
+//   }}
+// }
--- gladiator2.js
+++ gladiator2.js
@@ -1,75 +1,59 @@
-window.addEventListener("keydown", checkKeyPressed1, false);
-window.addEventListener("keydown", checkKeyPressed2, false);
+window.addEventListener("keydown", player1, false);
+window.addEventListener("keydown", player2, false);
+
+alert("Player 1 presses A to attack. Player 2 presses Enter to attack. Don't stop! Click 'Close' when you're ready!");
 
 
+function player1(evnt1) {
+    var bar2 = document.getElementById("myBar2");
+    // determines where it starts at
+    if (evnt1.keyCode == "65") {
+        var randomone = (Math.floor((Math.random() * 10) + 5));
+        bar2.value -= randomone;
+        var width1 = bar2.value;
+        bar2.style.width = width1 + '%'; // doesnt change it
+        document.getElementById("label2").innerHTML = width1 * 1 + '%'; //doesnt change it
+        if (bar2.value <= 0) {
+            // this is player 1 attacking player 2
+            document.getElementById("content2").innerHTML = 'Queen Marceline <br/> Loses!';
+            document.getElementById("content1").innerHTML = 'Princess Bubblegum <br/> Wins!';
+              alert("Congratulations, Princess Bubblegum! Player 1 wins!");
+              location.reload();
 
-
-
-// function attack1() {
-function checkKeyPressed1(evnt) {
-    var bar = document.getElementById("myBar2");
-    var width = 100; // determines where it starts at
-    if (evnt.keyCode == "65") {
-      var random=(Math.floor((Math.random() * 20) + 10))
-        width -= random;
-        bar.style.width = width + '%'; // doesnt change it
-        document.getElementById("label2").innerHTML = width * 1 + '%'; //doesnt change it
-
-        {
-            if (width <= 0) {
-                // this is player 1 attacking player 2
-                document.getElementById("content2").innerHTML = 'Queen Marceline <br/> Loses!';
-                document.getElementById("content1").innerHTML = 'Princess Bubblegum <br/> Wins!';
-            }
-
+            // swal({
+            //     title: "Congratulations, Princess Bubblegum!",
+            //     text: "Player 1, You Win!",
+            //     imageUrl: "bubblegumwon.gif"
+            // });
         }
-        }
 
+    }
 }
 
-function checkKeyPressed2(evnt) {
-    var bar = document.getElementById("myBar1");
-    var width = 100; // determines where it starts at
-    if (evnt.keyCode == "13") {
-      var random=(Math.floor((Math.random() * 20) + 10))
-        width -= random;
-        bar.style.width = width + '%'; // doesnt change it
-        document.getElementById("label1").innerHTML = width * 1 + '%'; //doesnt change it
+function player2(evnt2) {
+    var bar1 = document.getElementById("myBar1");
+    // determines where it starts at
+    if (evnt2.keyCode == "13") {
+        var randomtwo = (Math.floor((Math.random() * 10) + 5));
+        bar1.value -= randomtwo;
+        var width2 = bar1.value;
+        bar1.style.width = width2 + '%'; // doesnt change it
+        document.getElementById("label1").innerHTML = width2 * 1 + '%'; //doesnt change it
+        if (bar1.value <= 0) {
+            // this is player 2 attacking player 1
+            document.getElementById("content2").innerHTML = 'Queen Marceline <br/> Wins!';
+            document.getElementById("content1").innerHTML = 'Princess Bubblegum <br/> Loses!';
+              alert("Congratulations, Queen Marceline! Player 2 wins!");
+              location.reload();
 
-        {
-            if (width <= 0) {
-                // this is player 1 attacking player 2
-                document.getElementById("content2").innerHTML = 'Princess Bubblegum <br/> Loses!';
-                document.getElementById("content1").innerHTML = 'Queen Marceline <br/> Wins!';
-            }
+
+          // swal({
+          //         title: "Congratulations, Queen Marceline!",
+          //         text: "Player 2, You Win!",
+          //         imageUrl: "marcelinewon.gif"
+          //     });
 
         }
-        }
 
+    }
 }
-
-//
-// // function attack2() {
-// function checkKeyPressed2(evnt) {
-//     if (evnt.keyCode == "13") {
-//
-//   var bar = document.getElementById("myBar1");
-//   var width = 100;
-//   var id = setInterval(frame2,1000);
-//   function frame2() {
-//     if (width <= 0) {
-// // this is player 2 attacking player1
-//       document.getElementById("content1").innerHTML='Princess Bubblegum<br/> Loses!';
-//       document.getElementById("content2").innerHTML='Queen Marceline <br/> Wins!';
-//       // clearInterval(frame1);
-//       // break;
-//     } else {
-//
-//       width -=(Math.floor((Math.random() * 20) + 10));
-//       bar.style.width = width + '%';
-//       document.getElementById("label1").innerHTML = width * 1  + '%';
-//       clearInterval(frame1);
-//     }
-//
-//   }}
-// }
